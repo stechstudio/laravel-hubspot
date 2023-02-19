209e90b4e98f816c3bc239d6c38313ddffb281db
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2023-02-19 11:00:06 +0000

    Add docblock for IDE support to Deal

--- src/Crm/Deal.php
+++ src/Crm/Deal.php
@@ -2,8 +2,21 @@
 
 namespace STS\HubSpot\Crm;
 
+use STS\HubSpot\Api\Association;
+use STS\HubSpot\Api\Collection;
 use STS\HubSpot\Api\Model;
 
+/**
+ * @method Association contacts();
+ * @method Association companies();
+ * @method Association line_items();
+ * @property-read LineItems|null $line_item;
+ * @property-read Collection $line_items;
+ * @property-read Contact|null $contact;
+ * @property-read Collection $contacts;
+ * @property-read Company|null $company;
+ * @property-read Collection $companies;
+ */
 class Deal extends Model
 {
     protected string $type = "deals";
