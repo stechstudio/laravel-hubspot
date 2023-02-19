1708d0e559f462292c8a1fa4a1be83cc66144f32
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2023-02-19 11:17:20 +0000

    update docblocks - deal & quote

--- src/Crm/Deal.php
+++ src/Crm/Deal.php
@@ -7,15 +7,18 @@ use STS\HubSpot\Api\Collection;
 use STS\HubSpot\Api\Model;
 
 /**
- * @method Association contacts();
  * @method Association companies();
+ * @method Association contacts();
  * @method Association line_items();
- * @property-read LineItems|null $line_item;
- * @property-read Collection $line_items;
- * @property-read Contact|null $contact;
- * @property-read Collection $contacts;
+ * @method Association quotes();
  * @property-read Company|null $company;
  * @property-read Collection $companies;
+ * @property-read Contact|null $contact;
+ * @property-read Collection $contacts;
+ * @property-read LineItems|null $line_item;
+ * @property-read Collection $line_items;
+ * @property-read Quote|null $quote;
+ * @property-read Collection $quotes;
  */
 class Deal extends Model
 {
