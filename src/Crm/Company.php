bd35ad3c85bae02bc512ba8a52455164f2034cb7
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2023-02-19 11:41:20 +0000

    added more relation docblocks for ide-autocomplete

--- src/Crm/Company.php
+++ src/Crm/Company.php
@@ -2,8 +2,18 @@
 
 namespace STS\HubSpot\Crm;
 
+use STS\HubSpot\Api\Association;
+use STS\HubSpot\Api\Collection;
 use STS\HubSpot\Api\Model;
 
+/**
+ * @method Association contacts();
+ * @method Association deals();
+ * @property-read Contact|null $contact;
+ * @property-read Collection $contacts;
+ * @property-read Deal|null $deal;
+ * @property-read Collection $deals;
+ */
 class Company extends Model
 {
     protected string $type = "companies";
