68c1bcd6711f04576f7db66191a32602f60b64c6
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2023-02-19 11:04:20 +0000

    Add docblock for IDE support to LineItem

--- src/Crm/LineItems.php
+++ src/Crm/LineItems.php
@@ -2,8 +2,18 @@
 
 namespace STS\HubSpot\Crm;
 
+use STS\HubSpot\Api\Association;
+use STS\HubSpot\Api\Collection;
 use STS\HubSpot\Api\Model;
 
+/**
+ * @method Association deals();
+ * @method Association quotes();
+ * @property-read Deal|null $deal;
+ * @property-read Collection $deals;
+ * @property-read Quote|null $quote;
+ * @property-read Collection $quotes;
+ */
 class LineItems extends Model
 {
     protected string $type = "line_items";
